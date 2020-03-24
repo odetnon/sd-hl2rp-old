cc1a7f9847146d971f09b5795c4ccedec4fdec2d
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-03-24 22:01:37 +0000

    Update sh_water.lua

--- plugins/needs/items/consumables/sh_water.lua
+++ plugins/needs/items/consumables/sh_water.lua
@@ -1,6 +1,6 @@
 
-ITEM.name = "Breen's Water"
-ITEM.model = Model("models/props_junk/popcan01a.mdl")
-ITEM.description = "A blue aluminium can of plain water."
+ITEM.name = "Union Water"
+ITEM.model = Model("models/props_lunk/popcan01a.mdl")
+ITEM.description = "A blue aluminium can of plain Union water. It tastes like swimming pool water."
 ITEM.thirst = 25
 ITEM.stamina = 15
