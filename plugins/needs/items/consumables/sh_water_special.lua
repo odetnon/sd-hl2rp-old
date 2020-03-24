c9aea8cf487ffd3a3efa6159374261ac2533c578
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-03-24 22:04:43 +0000

    Update and rename sh_water_special.lua to sh_water_sparkling.lua

--- plugins/needs/items/consumables/sh_water_special.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Special Breen's Water"
-ITEM.model = Model("models/props_junk/popcan01a.mdl")
-ITEM.skin = 2
-ITEM.description = "A yellow aluminium can of water that seems a bit more viscous than usual."
-ITEM.thirst = 45
-ITEM.stamina = 50
