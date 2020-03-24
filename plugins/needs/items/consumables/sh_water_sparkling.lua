907a4a09ab1c44d199576667324ec2e0b2a17185
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-03-24 22:03:12 +0000

    Update and rename sh_water_sparkling.lua to sh_water_flavoured.lua

--- plugins/needs/items/consumables/sh_water_sparkling.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Sparkling Breen's Water"
-ITEM.model = Model("models/props_junk/popcan01a.mdl")
-ITEM.skin = 1
-ITEM.description = "A red aluminium can of carbonated water."
-ITEM.thirst = 35
-ITEM.stamina = 25
