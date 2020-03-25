3b6f2a67072813e1a91fd7537175f853025ea824
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-03-25 16:52:05 +0000

    Update sh_cp_mask.lua

--- schema/items/outfit/sh_cp_mask.lua
+++ schema/items/outfit/sh_cp_mask.lua
@@ -1,17 +1,19 @@
+-- NOTICE: ITEM MADE REDUNDANT! LEFT IN AS TEMPLATE
 
-ITEM.name = "Civil Protection Mask"
-ITEM.description = "A mask with a fitted respirator and voice modulator"
-ITEM.model = "models/dpfilms/metropolice/props/generic_gasmask.mdl"
 
-ITEM.replacements = "models/dpfilms/metropolice/hdpolice.mdl"
-ITEM.isRespirator = true
-
-function ITEM:CanEquipOutfit()
-	local clientModel = self:GetOwner():GetModel()
-
-	if (clientModel:find("/metropolice_")) then
-		return true
-	end
-
-	return false
-end
+--ITEM.name = "Civil Protection Mask"
+--ITEM.description = "A mask with a fitted respirator and voice modulator"
+--ITEM.model = "models/dpfilms/metropolice/props/generic_gasmask.mdl"
+--
+--ITEM.replacements = "models/dpfilms/metropolice/hdpolice.mdl"
+--ITEM.isRespirator = true
+--
+--function ITEM:CanEquipOutfit()
+--	local clientModel = self:GetOwner():GetModel()
+--
+--	if (clientModel:find("/metropolice_")) then
+--		return true
+--	end
+-
+--	return false
+--end
