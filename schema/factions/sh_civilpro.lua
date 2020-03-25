06ab5d5da0ef2521306edc404dc4c4a4b39e0d05
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-03-25 16:50:32 +0000

    Update sh_civilpro.lua

--- schema/factions/sh_civilpro.lua
+++ schema/factions/sh_civilpro.lua
@@ -2,23 +2,7 @@
 FACTION.name = "Civil Protection"
 FACTION.description = ""
 FACTION.color = Color(50, 150, 250)
-FACTION.models = {
-	"models/kake/metropolice_female01.mdl",
-	"models/kake/metropolice_female02.mdl",
-	"models/kake/metropolice_female03.mdl",
-	"models/kake/metropolice_female04.mdl",
-	"models/kake/metropolice_female06.mdl",
-	"models/kake/metropolice_female06_naomi.mdl",
-	"models/kake/metropolice_male01.mdl",
-	"models/kake/metropolice_male02.mdl",
-	"models/kake/metropolice_male03.mdl",
-	"models/kake/metropolice_male04.mdl",
-	"models/kake/metropolice_male05.mdl",
-	"models/kake/metropolice_male06.mdl",
-	"models/kake/metropolice_male07.mdl",
-	"models/kake/metropolice_male09.mdl",
-	"models/kake/metropolice_male09_hair.mdl"
-}
+FACTION.models = {"models/cultist/hl_a/metropolice/metrocop.mdl"}
 FACTION.isGloballyRecognized = false
 FACTION.runSounds = {[0] = "NPC_CombineS.RunFootstepLeft", [1] = "NPC_CombineS.RunFootstepRight"}
 FACTION.channels = {
@@ -34,7 +18,6 @@ function FACTION:OnCharacterCreated(client, character)
 	inventory:Add("stunstick", 1)
 	inventory:Add("usp_match", 1)
 	inventory:Add("9mm_ammo", 1)
-	inventory:Add("cp_mask", 1)
 end
 
 function FACTION:GetDefaultName(client)
