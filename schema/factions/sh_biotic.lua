86944d447e950484077c48186dee72fd3c5e419e
wowmod <mrsonyplays@gmail.com>
2020-03-25 11:42:44 -0500

    Update sh_biotic.lua

--- schema/factions/sh_biotic.lua
+++ schema/factions/sh_biotic.lua
@@ -6,4 +6,8 @@ FACTION.models = {
 	"models/vortigaunt_slave.mdl"
 }
 
+function FACTION:GetDefaultName(client)
+	return "CMB:BIOTIC."..string.format(math.random(1, 99999), "%05d"), true
+end
+
 FACTION_BIOTIC = FACTION.index
