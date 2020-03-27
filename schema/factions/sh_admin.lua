0db9b51248eba4f3eec82cf022357473cfb4ac58
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-03-27 13:57:43 +0000

    Update sh_admin.lua

--- schema/factions/sh_admin.lua
+++ schema/factions/sh_admin.lua
@@ -3,5 +3,6 @@ FACTION.name = "Server Administration"
 FACTION.description = ""
 FACTION.color = Color(184, 61, 53)
 FACTION.isGloballyRecognised = true
+FACTION.noNeeds = true
 
 FACTION_ADMIN = FACTION.index
