9db628f9230269d2ebe720deff3ed5aa687f6eaf
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-03-27 13:44:41 +0000

    Fixed spelling of recognised

--- schema/factions/sh_admin.lua
+++ schema/factions/sh_admin.lua
@@ -2,6 +2,6 @@
 FACTION.name = "Server Administration"
 FACTION.description = ""
 FACTION.color = Color(184, 61, 53)
-FACTION.isGloballyRecognized = true
+FACTION.isGloballyRecognised = true
 
 FACTION_ADMIN = FACTION.index
