b748a06291cafd9d0fa9501f6b051e05f3b078c4
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-01 13:53:00 +0100

    Update ix_combinelock.lua

--- entities/entities/ix_combinelock.lua
+++ entities/entities/ix_combinelock.lua
@@ -170,13 +170,13 @@ if (SERVER) then
 else
 	local glowMaterial = ix.util.GetMaterial("sprites/glow04_noz")
 	local color_green = Color(0, 255, 0, 255)
-	local color_blue = Color(0, 100, 255, 255)
+	local color_blue = Color(255, 50, 50, 255)
 	local color_red = Color(255, 50, 50, 255)
 
 	local lockColors = {
-		[1] = Color(200, 100, 0),
+		[1] = Color(255, 255, 255),
 		[2] = Color(255, 255, 255),
-		[3] = Color(0, 100, 200)
+		[3] = Color(255, 255, 255)
 	}
 
 	function ENT:Draw()
