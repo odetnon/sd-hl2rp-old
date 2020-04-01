5b2550a6640d29bce8161acd36399d8b11b8a029
wowmod <mrsonyplays@gmail.com>
2020-04-01 14:32:42 -0500

    Update sh_commands.lua

--- schema/sh_commands.lua
+++ schema/sh_commands.lua
@@ -79,6 +79,28 @@ do
 end
 
 do
+	local COMMAND = {}
+
+	function COMMAND:OnRun(client, arguments)
+		local data = {}
+			data.start = client:GetShootPos()
+			data.endpos = data.start + client:GetAimVector() * 96
+			data.filter = client
+		local target = util.TraceLine(data).Entity
+
+		if (IsValid(target) and target:IsPlayer() and target:IsRestricted()) then
+			if (!client:IsRestricted()) then
+				Schema:SearchPlayer(client, target)
+			else
+				return "@notNow"
+			end
+		end
+	end
+
+	ix.command.Add("CharSearch", COMMAND)
+end
+
+do
 	for cmd, cmdTbl in pairs(communityCommands) do
 		ix.command.Add(cmd, {
 			description = cmdTbl.desc,
