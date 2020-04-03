6a5b1f1fc34a8d22fd6c47917d0f616c5d4ea3dc
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-03 18:05:13 +0100

    Delete sh_ar2_ammo.lua

--- schema/items/ammo/sh_ar2_ammo.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Pulse Rounds"
-ITEM.description = "A cartrige filled with bullets of an foreign energy"
-ITEM.model = "models/items/combine_rifle_cartridge01.mdl"
-
-ITEM.ammo = "ar2"
-ITEM.ammoAmount = 30
