484c6aea470e540d20716c7701d8fd32665fd43b
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-03 18:11:48 +0100

    Delete sh_water_sparkling.lua

--- plugins/needs/items/consumables/sh_water_sparkling.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Sparkling Union Water"
-ITEM.model = Model("models/props_lunk/popcan01a.mdl")
-ITEM.skin = 2
-ITEM.description = "A yellow aluminium can of Sparkling Union water. It emits a fizz sound when you open it."
-ITEM.thirst = 45
-ITEM.stamina = 50
