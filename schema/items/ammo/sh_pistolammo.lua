2b4db0d6a60df6c8d3aac197627029dcf47ecca9
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-03 18:05:55 +0100

    Add files via upload

---
+++ schema/items/ammo/sh_pistolammo.lua
@@ -0,0 +1,7 @@
+ITEM.name = "9mm Pistol Bullets"
+ITEM.model = "models/Items/BoxSRounds.mdl"
+ITEM.ammo = "pistol" -- type of the ammo
+ITEM.ammoAmount = 30 -- amount of the ammo
+ITEM.ammoDesc = "A Box that contains %s of Pistol Ammo"
+ITEM.classes = {CLASS_EMP, CLASS_EOW}
+ITEM.flag = "V"
