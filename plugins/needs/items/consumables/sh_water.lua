887886fb8d43dc4c5e9e1d13cb9b718ea61e1628
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-03 18:12:06 +0100

    Delete sh_water.lua

--- plugins/needs/items/consumables/sh_water.lua
+++
@@ -1,6 +0,0 @@
-
-ITEM.name = "Union Water"
-ITEM.model = Model("models/props_lunk/popcan01a.mdl")
-ITEM.description = "A blue aluminium can of plain Union water. It tastes like swimming pool water."
-ITEM.thirst = 25
-ITEM.stamina = 15
