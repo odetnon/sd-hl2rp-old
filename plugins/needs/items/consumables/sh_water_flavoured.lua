276cd567dc27666c9ed8c697db30f017d9cf4431
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-03 18:12:00 +0100

    Delete sh_water_flavoured.lua

--- plugins/needs/items/consumables/sh_water_flavoured.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Flavoured Union Water"
-ITEM.model = Model("models/props_lunk/popcan01a.mdl")
-ITEM.skin = 1
-ITEM.description = "A red aluminium can of Flavoured Union Water. It has a peach flavour to it."
-ITEM.thirst = 35
-ITEM.stamina = 25
