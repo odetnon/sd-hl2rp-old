18e47f17987b5d516f488393903016ed2d81b5f7
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-04-07 23:16:43 +0100

    Create sh_backpack.lua

---
+++ schema/items/bags/sh_backpack.lua
@@ -0,0 +1,8 @@
+
+ITEM.name = "Backpack"
+ITEM.description = "A backpack used to carry stuff."
+ITEM.model = "models/props_junk/garbage_bag001a.mdl"
+ITEM.height = 2
+ITEM.width = 2
+ITEM.invHeight = 3
+ITEM.invWidth = 3
