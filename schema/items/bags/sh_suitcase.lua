f6007de6647fa57e8ea977529e08f4e8d6617dfa
EmpireDidNothingWrong <62622845+EmpireDidNothingWrong@users.noreply.github.com>
2020-04-07 23:14:07 +0100

    Update sh_suitcase.lua

--- schema/items/bags/sh_suitcase.lua
+++ schema/items/bags/sh_suitcase.lua
@@ -1,4 +1,8 @@
 
 ITEM.name = "Suitcase"
-ITEM.description = "A small suitcase."
+ITEM.description = "A small suitcase used to hold the bare essentials."
 ITEM.model = "models/weapons/w_suitcase_passenger.mdl"
+ITEM.height = 2
+ITEM.width = 2
+ITEM.invHeight = 2
+ITEM.invWidth = 4
