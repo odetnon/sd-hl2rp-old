25d3d6f9be24c0ff8831f9653788b04de5837207
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-08 12:01:44 +0100

    Update sh_overwatch_soldier.lua

--- schema/classes/sh_overwatch_soldier.lua
+++ schema/classes/sh_overwatch_soldier.lua
@@ -6,7 +6,7 @@ function CLASS:OnCanBe(client)
 	local name = client:Name()
 	local bStatus = false
 
-	for k, v in ipairs({ "ECHO"}) do
+	for k, v in ipairs({ "OWS"}) do
 		if (Schema:IsCombineRank(name, v)) then
 			bStatus = true
 
