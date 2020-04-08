82d81d0a2bca4dd38e112a6f36432de138a7d57c
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-08 11:52:55 +0100

    Update sh_elite_overwatch_soldier.lua

--- schema/classes/sh_elite_overwatch_soldier.lua
+++ schema/classes/sh_elite_overwatch_soldier.lua
@@ -2,11 +2,28 @@ CLASS.name = "Elite Overwatch Soldier"
 CLASS.faction = FACTION_OTA
 CLASS.isDefault = false
 
+
+function CLASS:OnCanBe(client)
+	local name = client:Name()
+	local bStatus = false
+
+	for k, v in ipairs({ "KING"}) do
+		if (Schema:IsCombineRank(name, v)) then
+			bStatus = true
+
+			break
+		end
+	end
+
+	return bStatus
+end
+
+
 function CLASS:OnSet(client)
 	local character = client:GetCharacter()
 
 	if (character) then
-		character:SetModel("models/bloocobalt/combine/combine_e.mdl")
+		character:SetModel("models/characters/combine_soldier/jqblk/combine_s_super.mdl")
 	end
 end
 
