ee753e570e911d781e0b32974141bd77695737e6
DevAppeared <58948641+DevAppeared@users.noreply.github.com>
2020-04-08 11:52:15 +0100

    Update sh_overwatch_soldier.lua

--- schema/classes/sh_overwatch_soldier.lua
+++ schema/classes/sh_overwatch_soldier.lua
@@ -2,6 +2,22 @@ CLASS.name = "Overwatch Soldier"
 CLASS.faction = FACTION_OTA
 CLASS.isDefault = true
 
+function CLASS:OnCanBe(client)
+	local name = client:Name()
+	local bStatus = false
+
+	for k, v in ipairs({ "ECHO"}) do
+		if (Schema:IsCombineRank(name, v)) then
+			bStatus = true
+
+			break
+		end
+	end
+
+	return bStatus
+end
+
+
 function CLASS:OnSet(client)
 	local character = client:GetCharacter()
 
