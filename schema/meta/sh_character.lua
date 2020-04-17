c101a6f6ba80884211e822bb015bcbe1037a4d7d
wowm0d <mrsonyplays@gmail.com>
2020-04-17 13:52:44 -0500

    Update sh_character.lua

--- schema/meta/sh_character.lua
+++ schema/meta/sh_character.lua
@@ -2,9 +2,11 @@
 local characterMeta = ix.meta.character
 
 function characterMeta:IsCombine()
-	return (self:GetFaction() == FACTION_CP or self:GetFaction() == FACTION_OTA or self:GetFaction() == FACTION_DISP)
+	local faction = self:GetFaction()
+	return faction == FACTION_CP or faction == FACTION_OTA or faction == FACTION_DISP
 end
 
 function characterMeta:IsDispatch()
-	return (self:GetFaction() == FACTION_DISP)
+	local faction = self:GetFaction()
+	return faction == FACTION_DISP
 end
