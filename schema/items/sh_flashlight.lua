c5ad16c006ba237eb9687eb0e54fa0312a9e54f5
Dev <58948641+DevAppeared@users.noreply.github.com>
2020-06-15 15:48:31 +0100

    Removed old OTA classes, add in new working flashlight item.

---
+++ schema/items/sh_flashlight.lua
@@ -0,0 +1,5 @@
+
+ITEM.name = "Flashlight"
+ITEM.description = "An old flashlight that emits a powerful beam of light."
+ITEM.model = "models/pg_props/pg_obj/pg_flashlight.mdl"
+ITEM.bDropOnDeath = true
