4978af69ed17c29c9e0fabdbec23ecf48d041606
Dev <58948641+DevAppeared@users.noreply.github.com>
2020-07-07 20:23:39 +0100

    Fix biotic ration contents

--- schema/items/rations/sh_ration_biotic.lua
+++ schema/items/rations/sh_ration_biotic.lua
@@ -2,5 +2,5 @@
 ITEM.name = "Biotic Grade Ration Unit"
 ITEM.model = Model("models/weapons/w_packatb.mdl")
 ITEM.description = "A yellow and green shrink-wrapped packet that should contain biotic supplements and a can of liquid sustenance."
-ITEM.items = {"standardsupp","water"}
+ITEM.items = {"bioticsupp","waterbiotic"}
 ITEM.bDropOnDeath = true
