9b04fa4abb5ab58cb7b60073c274ebd8065f5aa6
DevAppeared <oscardetnon@gmail.com>
2020-07-26 14:49:45 +0100

    vort footsteps

--- schema/factions/sh_biotic.lua
+++ schema/factions/sh_biotic.lua
@@ -6,6 +6,7 @@ FACTION.models = {"models/vortigaunt_slave.mdl"}
 FACTION.weapons = {"swep_vortigaunt_sweep"}
 FACTION.isDefault = false
 FACTION.isGloballyRecognized = false
+FACTION.runSounds = {[0] = "npc/vort/vort_foot1.wav", [1] = "npc/vort/vort_foot2.wav"}
 
 function FACTION:GetDefaultName(client)
 	return "CMB:BIOTIC."..string.format(math.random(1, 99999), "%05d"), true
